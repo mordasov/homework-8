c8cf1235878eb307800b751c469151307dae47d0
mss88 <mss88@list.ru>
2024-04-15 18:54:31 +0300

    Done homework-8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,74 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task-1
+        int[] arrayInt = new int[] {1, 2, 3};
+        double[] arrayDouble = {1.57, 7.654, 9.986};
+        boolean[] arrayBoolean = {true, false, true, false, false};
+
+        //task-2
+        for(int i = 0; i < arrayInt.length; i++) {
+            System.out.print(arrayInt[i]);
+            if(i == arrayInt.length - 1) {
+                System.out.println();
+                break;
+            }
+            System.out.print(", ");
+        }
+
+        for(int i = 0; i < arrayDouble.length; i++) {
+            System.out.print(arrayDouble[i]);
+            if(i == arrayDouble.length - 1) {
+                System.out.println();
+                break;
+            }
+            System.out.print(", ");
+        }
+
+        for(int i = 0; i < arrayBoolean.length; i++) {
+            System.out.print(arrayBoolean[i]);
+            if(i == arrayBoolean.length - 1) {
+                System.out.println("\n");
+                break;
+            }
+            System.out.print(", ");
+        }
+
+        //task-3
+        for(int i = arrayInt.length - 1; i >= 0; i--) {
+            System.out.print(arrayInt[i]);
+            if(i == 0) {
+                System.out.println();
+                break;
+            }
+            System.out.print(", ");
+        }
+
+        for(int i = arrayDouble.length - 1; i >= 0; i--) {
+            System.out.print(arrayDouble[i]);
+            if(i == 0) {
+                System.out.println();
+                break;
+            }
+            System.out.print(", ");
+        }
+
+        for(int i = arrayBoolean.length - 1; i >= 0; i--) {
+            System.out.print(arrayBoolean[i]);
+            if(i == 0) {
+                System.out.println("\n");
+                break;
+            }
+            System.out.print(", ");
+        }
+
+        //task-4
+        for(int i = 0; i < arrayInt.length; i++){
+            if(arrayInt[i] % 2 != 0) arrayInt[i] += 1;
+            //System.out.print(arrayInt[i] + ", "); //ещё вариант - распечатать массив
+        }
+        System.out.println(Arrays.toString(arrayInt));
     }
 }
